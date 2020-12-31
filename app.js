f98576f15ac3ab2eb27495c09c713d5b06753255
Murat Demirtas <73036794+muratdemirtas2001@users.noreply.github.com>
2020-12-31 10:29:09 +0000

    adding queries and eventlisteners

--- app.js
+++ app.js
@@ -3,3 +3,16 @@
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
 
+const modalBtn= document.querySelector('.modal-btn');
+const closeBtn= document.querySelector('.close-btn');
+const modalOverlay=document.querySelector('.modal-overlay');
+
+modalBtn.addEventListener('click',()=>{
+    modalOverlay.classList.toggle('open-modal');
+
+})
+
+closeBtn.addEventListener('click',()=>{
+    modalOverlay.classList.toggle('open-modal')
+
+})
